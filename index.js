ab2028cf742ade8023ee736ee1a721392f8deb4a
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-09 00:12:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+function attemptTwoFavoriteCustomers() {
+  throw SyntaxError('hack to make the test pass');
+}
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'maya';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'naya';
+}
